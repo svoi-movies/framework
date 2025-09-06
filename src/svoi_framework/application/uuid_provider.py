d5cf49fb9b23d2a57e9673f34c567b8ab2521fb2
Artem Burenin <burenin@mindbox.cloud>
2025-09-07 00:34:13 +0300

    Add default providers

---
+++ src/svoi_framework/application/uuid_provider.py
@@ -0,0 +1,14 @@
+import abc
+import uuid
+
+
+class UUIDProvider(abc.ABC):
+
+    @abc.abstractmethod
+    def generate(self) -> uuid.UUID: ...
+
+
+class DefaultUUIDProvider(UUIDProvider):
+
+    def generate(self) -> uuid.UUID:
+        return uuid.uuid4()
