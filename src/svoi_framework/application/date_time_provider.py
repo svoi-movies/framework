05c54ea6c9fd808e0953dbad3fa066271354de3c
Artem Burenin <burenin.aa@yandex.ru>
2025-09-07 00:36:06 +0300

    Add default providers (#3)
    * Add default providers
    
    * bump
    
    * bump
    
    ---------
    
    Co-authored-by: Artem Burenin <burenin@mindbox.cloud>

---
+++ src/svoi_framework/application/date_time_provider.py
@@ -0,0 +1,16 @@
+import abc
+from datetime import datetime
+
+import pytz
+
+
+class DateTimeProvider(abc.ABC):
+
+    @abc.abstractmethod
+    def utc_now(self) -> datetime: ...
+
+
+class DefaultDateTimeProvider(DateTimeProvider):
+
+    def utc_now(self) -> datetime:
+        return datetime.now(pytz.UTC)
