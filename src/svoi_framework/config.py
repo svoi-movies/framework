7beac3539a03ce84ac7b1ca9b19b53d5d4d131a2
Artem Burenin <burenin@mindbox.cloud>
2025-09-07 13:20:25 +0300

    Add instrumentation

---
+++ src/svoi_framework/config.py
@@ -0,0 +1,10 @@
+import yaml
+
+from pathlib import Path
+from pydantic import BaseModel
+
+
+def read_config[T: BaseModel](model: type[T], file_path: Path) -> T:
+    with open(file_path, "r") as file:
+        raw_data = yaml.full_load_all(file)
+        return model.model_validate(raw_data)
