48b88bd3274e7aeda0abad03defb5b6b6336039c
Artem Burenin <burenin@mindbox.cloud>
2025-09-07 15:38:41 +0300

    Fix read_config

--- src/svoi_framework/config.py
+++ src/svoi_framework/config.py
@@ -6,5 +6,5 @@ from pydantic import BaseModel
 
 def read_config[T: BaseModel](model: type[T], file_path: Path) -> T:
     with open(file_path, "r") as file:
-        raw_data = yaml.full_load_all(file)
+        raw_data = yaml.safe_load(file)
         return model.model_validate(raw_data)
